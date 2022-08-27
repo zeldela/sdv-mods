e1af0c68a7a33da896a19494c1bd92bedaa0e6ef
zeldela <delagomen@gmail.com>
2022-08-27 19:15:57 -0400

    adding mods

---
+++ sdv-mods/JustMyLuck/LuckHUD.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace JustMyLuck
+{
+    internal class LuckHUD      
+    {
+        private readonly IMonitor Monitor;
+
+        public LuckHUD(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
+
+        internal string GetFortune(Farmer who)
+        {
+            double dailyLuck = who.DailyLuck;
+            string fortuneID;
+
+            if (dailyLuck < -0.07)
+            {
+                fortuneID = "13192";
+            }
+            else if (dailyLuck < -0.02)
+            {
+                fortuneID = "13193";
+            }
+            else if (dailyLuck == 0)
+            {
+                fortuneID = "13201";
+            }
+            else if (dailyLuck <= 0.02)
+            {
+                fortuneID = "13200";
+            }
+            else if (dailyLuck <= 0.07)
+            {
+                fortuneID = "13199";
+            }
+            else
+            {
+                fortuneID = "13198";
+            }
+
+            string fortuneTV = Game1.content.LoadString($"Strings\\StringsFromCSFiles:TV.cs.{fortuneID}");
+            string[] delimiterChars = { "! ", "!", ". ", "." };
+            string[] words = fortuneTV.Split(delimiterChars, System.StringSplitOptions.RemoveEmptyEntries);
+            string fortune = "";
+
+            foreach (var word in words)
+            {
+                fortune = $"{fortune}{word}.\n";
+            }
+
+            double fortuneLuck = Math.Truncate((dailyLuck * 100 * 100)) / 100;
+
+            if (dailyLuck < 0)
+            {
+                fortune = $"{fortune}Luck score: -{fortuneLuck}%.";
+            }
+            else
+            {
+                fortune = $"{fortune}Luck score: +{fortuneLuck}%.";
+            }
+
+            return fortune;
+        }
+
+        internal Color GetFortuneColor(Farmer who)
+        {
+            double dailyLuck = who.DailyLuck;
+            if (dailyLuck < -0.03)
+            {
+                return new Color(222, 161, 144, 255);
+            }
+            else if (dailyLuck > 0.03)
+            {
+                return new Color(130, 190, 145, 255);
+            }
+            else
+            {
+                return Color.White;
+            }
+
+        }
+
+        internal ClickableTextureComponent GetLuckIcon()
+        {
+            int zoom = 3;
+            int x = Game1.uiViewport.Width - 300;
+            int y = 165;
+            ClickableTextureComponent luckIcon = new ClickableTextureComponent(
+                    new Rectangle(x, y, 10 * zoom, 10 * zoom),
+                    Game1.mouseCursors,
+                    new Rectangle(381, 361, 10, 10),
+                    zoom);
+            return luckIcon;
+        }
+
+    }
+}
+
---
+++ sdv-mods/JustMyLuck/ModEntry.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace JustMyLuck
+{
+    public class ModEntry : Mod
+    {
+        string fortuneMessage;
+        Color fortuneColor;
+        ClickableTextureComponent luckIcon;
+        LuckHUD luckHUD;
+        bool hover; 
+        
+
+        public override void Entry(IModHelper helper)
+        {
+            luckHUD = new LuckHUD(this.Monitor);
+            ModConfig Config = this.Helper.ReadConfig<ModConfig>();
+
+            if (!Config.Disable)
+            {
+                helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+                helper.Events.Display.RenderingHud += this.OnRenderingHud;
+                helper.Events.Input.CursorMoved += this.OnCursorMoved;
+                helper.Events.Display.Rendered += this.OnRendered;
+
+            }
+
+        }
+
+        internal void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            fortuneMessage = luckHUD.GetFortune(Game1.player);
+            fortuneColor = luckHUD.GetFortuneColor(Game1.player);
+            this.Monitor.Log($"OnDayStarted luck for {Game1.player.Name}:\n{fortuneMessage}", LogLevel.Debug);
+            luckIcon = luckHUD.GetLuckIcon();
+
+        }
+
+        internal void OnRenderingHud(object sender, RenderingHudEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (Context.IsPlayerFree)
+            {
+                luckIcon.draw(Game1.spriteBatch, fortuneColor, 1);
+            }
+        }
+
+        internal void OnRendered(object sender, RenderedEventArgs e)
+        {
+            if (Context.IsPlayerFree && hover)
+            {
+                IClickableMenu.drawHoverText(Game1.spriteBatch, fortuneMessage, Game1.smallFont);
+            }
+        }
+
+        internal void OnCursorMoved(object sender, CursorMovedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (Context.IsPlayerFree && luckIcon != null)
+            {
+                if (luckIcon.containsPoint(Game1.getMouseX(), Game1.getMouseY()))
+                {
+                    hover = true; 
+                }
+                else
+                {
+                    hover = false;
+                }
+            }
+        }
+
+    }
+}
+
